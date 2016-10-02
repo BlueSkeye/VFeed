6fde0b3aa6d76c991e661b0fa9bf8bea8a708b20
BlueSkeye <bsgithub@gmail.com>
2016-10-02 13:59:15 +0200

    Program skeleton including args parsing

---
+++ SSUpdate/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data.SQLite;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSUpdate
+{
+    public static class Program
+    {
+        private static void DisplayUsage(string programName)
+        {
+            Console.WriteLine("{0} [-f <database path>|-h]");
+            Console.WriteLine();
+            Console.WriteLine("-f : path to vFeed database.");
+            Console.WriteLine("-h : display this help notice.");
+            return;
+        }
+
+        private static void DisplayVersion(out string name)
+        {
+            Assembly mainAssembly = Assembly.GetEntryAssembly();
+            AssemblyName mainAssemblyName = mainAssembly.GetName();
+
+            name = mainAssemblyName.Name;
+            Console.WriteLine("{0} v{1}.", name, mainAssemblyName.Version);
+            return;
+        }
+
+        public static int Main(string[] args)
+        {
+            int result = 0;
+            string programName;
+            DisplayVersion(out programName);
+
+            if (!ParseArgs(args)) {
+                _displayUsage = true;
+            }
+            if (_displayUsage) {
+                DisplayUsage(programName);
+                return result;
+            }
+            return result;
+        }
+
+        private static bool ParseArgs(string[] args)
+        {
+            for(int argIndex = 0; args.Length > argIndex; argIndex++) {
+                string scannedArg = args[argIndex];
+                bool unrecognizedArgument = false;
+                if ((2 > scannedArg.Length) || !scannedArg.StartsWith("-")) {
+                    unrecognizedArgument = true;
+                }
+                else {
+                    switch (scannedArg.Substring(1).ToUpper()) {
+                        case "F":
+                            string dbPath = TryGetAdditionalArgument(args, ref argIndex);
+                            if (null == dbPath) { return false; }
+                            _database = new FileInfo(dbPath);
+                            if (!_database.Exists) {
+                                Console.WriteLine("Database '{0}' not found.", _database.FullName);
+                                return false;
+                            }
+                            using (FileStream data = TryOpenDatabase()) {
+                                if (null == data) { return false; }
+                            }
+                            break;
+                        default:
+                            unrecognizedArgument = true;
+                            break;
+                    }
+                }
+                if (unrecognizedArgument) {
+                    Console.WriteLine("Unrecognized argument '{0}'.",
+                    scannedArg);
+                    return false;
+                }
+            }
+            _connectionString = string.Format("Data Source={0};Version=3;",
+                _database.FullName);
+            return true;
+        }
+
+        private static string TryGetAdditionalArgument(string[] from, ref int index)
+        {
+            string currentArgument = from[index];
+            if (from.Length <= ++index) {
+                Console.WriteLine("Additional argument missing after {0}.",
+                    currentArgument);
+                return null;
+            }
+            return from[index];
+        }
+
+        private static FileStream TryOpenDatabase()
+        {
+            try {
+                return File.Open(_database.FullName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error occurred while opening database '{0}' : {1}",
+                    _database.FullName, e.Message);
+                return null;
+            }
+        }
+
+        private static string _connectionString;
+        private static FileInfo _database;
+        private static bool _displayUsage;
+    }
+}
---
+++ SSUpdate/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SSUpdate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SSUpdate")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e62d8c43-5970-4fef-96b7-921dca8b6717")]
+[assembly: AssemblyVersion("1.0.*")]
